1db0f9c63b9bbb2a0012b153cf1f909c8652be20
Nickforall <hello@nickforall.nl>
2016-10-08 14:35:45 +0200

    Working on Carbon Bytecode

---
+++ compiler/bytecode/image.js
@@ -0,0 +1,7 @@
+var Image = function () {
+    this.strings = [];
+    this.main = [];
+    this.blocks = [];
+};
+
+module.exports = Image;
---
+++ compiler/bytecode/opcodes.js
@@ -0,0 +1,14 @@
+module.exports = {
+    null: 0,
+    pushnum: 1,
+    pushstr: 2,
+    pusharr: 3,
+    pushflt: 4,
+    valadd: 5,
+    nummlp: 6,
+    numdiv: 7,
+    numsub: 8,
+    nummod: 9,
+    pushtrue: 10,
+    pushfalse: 11
+}
