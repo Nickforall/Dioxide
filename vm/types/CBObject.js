346db06c3240f7c5f974e68d388f1e2a18041061
Nickforall <hello@nickforall.nl>
2017-03-06 20:00:00 +0100

    Object notation

---
+++ vm/types/CBObject.js
@@ -0,0 +1,25 @@
+const CarbonBase = require("./CarbonBase");
+const CBNull = require("./CBNull");
+
+class CBObject extends CarbonBase {
+    constructor(obj) {
+        super("OBJECT");
+
+        this.content = obj
+    }
+
+    getProperty(prop) {
+        return this.content[prop] ? this.content[prop] : new CBNull();
+    }
+
+    setProperty(prop, value) {
+        if(value.typename == "NULL") {
+            this.content[prop] = undefined;
+        } else {
+            if(value._iscarbon) throw new Error("Prop");
+            this.content[prop] = value;
+        }
+    }
+}
+
+module.exports = CBObject;
